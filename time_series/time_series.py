f607e8d7183e5e82f6ed6b3653b040685b9fb25a
Khayyam Saleem <ksaleem@stevens.edu>
2017-06-08 16:45:12 -0600

    testing normalization of data in time_series.py

--- time_series/time_series.py
+++ time_series/time_series.py
@@ -1,5 +1,7 @@
 from random import randint
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Time_Series:
     def __init__(self, w, t, a, abtype):
@@ -8,7 +10,13 @@ class Time_Series:
         self.param = t
         self.imbalance = a
         self.abtype = abtype
-        self.x = range(w)
-        self.y = [0]*w
-        for i in self.x:
+        
+        mu, sigma = 0, 1
+        s = np.random.normal(mu, sigma, 1000)
 
+if __name__ == "__main__":
+    mu, sigma = 0, 0.1
+    s = np.random.normal(mu, sigma, 1000)
+    count, bins, ignored = plt.hist(s, 20, normed=True)
+    plt.plot(bins, 1/(sigma * np.sqrt(2 *np.pi)) * np.exp(-(bins-mu)**2 / (2*sigma**2)), linewidth=2, color = 'r')
+    plt.show()
