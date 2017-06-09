d26718d117d36521696f030b13103a8eb2707040
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-06-08 21:47:01 -0600

    modified tests so that they accept user input (for demo)

--- ccpr/tests/test_time_series.py
+++ ccpr/tests/test_time_series.py
@@ -1,5 +1,15 @@
 from ccpr.time_series import time_series as ast
+import matplotlib.pyplot as plt
 print("Usage: TimeSeries(window, param, imbalance_ratio, 'abtype')")
 
-upt = ast.TimeSeries(10, 0.105, 45, "uptrend")
-upt.to_csv()
+abtype = input("Enter abnormal pattern type: ")
+window = int(input("Enter window length: "))
+imbalance = int(input("Enter imbalance ratio: "))
+param = float(input("Enter parameter of abnormal pattern: "))
+
+upt = ast.TimeSeries(window, parameter, imbalance, abtype)
+#upt.to_csv()
+
+plt.plot(range(upt.get_window()), upt.get_norm()[0], 'r')
+plt.plot(range(upt.get_window()), upt.get_abnorm()[0], 'b')
+plt.show()
