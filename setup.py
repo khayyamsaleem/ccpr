d5405ae43911b9ad11d809e4f9e60f97a4e29370
Khayyam Saleem <ksaleem@stevens.edu>
2017-06-08 20:02:05 -0600

    added more to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name="ccpr",
       version="0.1",
       description="A library for control chart pattern recognition",
+      long_description=readme(),
       url="https://github.com/khayyamsaleem/ccpr",
       author="Khayyam Saleem",
       author_email="ksaleem@stevens.edu",
       license="MIT",
       packages=["ccpr"],
+      scripts=['bin/ccpr'],
       install_requires=[
           "matplotlib",
           "numpy"
